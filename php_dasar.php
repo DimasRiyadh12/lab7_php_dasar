08bbcdc0e596b59baaf71b0096184b81701cadf1
DimasRiyadh12 <dimasriyadh@gmail.com>
2022-05-18 17:52:00 +0700

    menambahkan file php_dasar.php

--- php_dasar.php
+++ php_dasar.php
@@ -1,108 +1,94 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
-        <meta charset="UTF-8">
-        <title>PHP Dasar</title>
+        <title>Form Input</title>
+        <meta charset="utf-8">
+
+        <!-- CSS -->
+        <style>
+        body {
+            width: 100%;
+            height: 100vh;
+            margin: 0;
+            font-family: tahoma;
+            font-size: 16px;
+        }
+        h1, p {
+            margin: 1em auto;
+            text-align: center;
+        }
+        form {
+            width: 60vw;
+            max-width: 500px;
+            min-width: 300px;
+            margin: 0 auto;
+            padding-bottom: 2em;
+            }
+        label {
+            display: block;
+            margin: 0.5rem 0;
+        }
+        button[type="submit"] {
+            display: block;
+            width: 60%;
+            margin: 1em auto;
+            height: 2em;
+            font-size: 1.1rem;
+            background-color: #e0daca;
+            border-color: white;
+            min-width: 300px;
+        }
+        </style>
     </head>
+
     <body>
-        <h1>Belajar PHP Dasar</h1>
-        <?php
-        echo "Hello World";
-        ?>
+        <h1 id="title">Survey Formulir Pekerjaan dan Gaji</h1>
+        <p id="description"><i>Melansir dari situs <a href="https://id.indeed.com/career/php-developer/salaries"> indeed.com</a></i></p>
 
-        <h1>Menggunakan Variable</h1>
-        <?php
-        $nim = "0411500400";
-        $nama = 'Abdullah';
-        echo "NIM : " . $nim . "<br>";
-        echo "Nama : $nama";
-        ?>
+        <form id="survey-form" action="" method="POST">
+            <fieldset>
+                <label>Nama: 
+                    <input type="text" name="nama" required/>
+                </label>
+                <label>Tanggal lahir: 
+                    <input type="date" name="date">
+                </label>
+                <label>Pekerjaan: 
+                    <label>
+                    <input type="radio" name="job" value="0"/>Database Administrator
+                    </label>
+                    <label>
+                    <input type="radio" name="job" value="1"/>Software Developer
+                    </label>
+                    <label>
+                    <input type="radio" name="job" value="2"/>Web Developer
+                    </label>
+                </label>
+                <button type="submit" name="submit">Kirim</button>
+            </fieldset>
+            <fieldset>
+                <?php
+                if( isset($_POST['submit']))
+                {
+                    $nama = $_POST['nama'];
+                    $date = $_POST['date'];
+                    $job = $_POST['job'];
 
-        <h1>Predefine Variable</h1>
-        <?php
-        echo 'Selamat Datang'. $_GET['nama'];
-        ?>
 
-        <h2>Form Input</h2>
-        <form method="post">
-            <label>Nama: </label>
-            <input type="text" name="nama">
-            <input type="submit" value="Kirim">
+                    $date_user = new DateTime($date);
+                    $today =  new DateTime('today');
+                    $usia = $today->diff($date_user)->y;
+
+                    $job_array = ["Database Administrator","Software Developer","Web Developer"];
+                    $salary_array = ["Rp. 5.300.000","Rp. 5.400.000","Rp. 4.800.000"];
+
+
+                    echo "Halo, ".$nama."<br>Kamu lahir pada tanggal ".$date.", Usia mu ".$usia." tahun";
+                    echo "<br>Pekerjaan yang kamu pilih adalah ".$job_array[(int)$job].", dengan penghasilan kurang lebih ".$salary_array[(int)$job];
+                }
+                ?>
+            </fieldset>
         </form>
-        <?php
-        echo 'Selamat Datang '. $_POST['nama'];
-        ?>
-
-        <h2>Operator</h2>
-        <?php
-        $gaji = 1000000;
-        $pajak = 0.1;
-        $thp = $gaji - ($gaji*$pajak);
-        echo "Gaji sebelum pajak = Rp. $gaji <br>";
-        echo "Gaji yang dibawa pulang = Rp. $thp";
-        ?>
-
-        <h2>Kondisi IF</h2>
-        <?php
-        $nama_hari = date("l");
-        if ($nama_hari == "Sunday") {
-        echo "Minggu";
-        } elseif ($nama_hari == "Monday") {
-        echo "Senin";
-        } else {
-        echo "Selasa";
-        }
-        ?>
-
-        <h2>Kondisi Switch</h2>
-        <?php
-        $nama_hari = date("l");
-        switch ($nama_hari) {
-            case "Sunday":
-                echo "Minggu";
-                break;
-            case "Monday":
-                echo "Senin";
-                break;
-            case "Tuesday":
-                echo "Selasa";
-                break;
-            default:
-                echo "Sabtu";
-        }
-        ?>
-
-        <h2>Perulangan For</h2>
-        <?php
-        echo "Perulangan 1 sampai 10 <br />";
-        for ($i=1; $i<=10; $i++) {
-            echo "Perulangan ke: " . $i . '<br />';
-        }
-        
-        echo "Perulangan Menurun dari 10 ke 1 <br />";
-        for ($i=10; $i>=1; $i--) {
-            echo "Perulangan ke: " . $i . '<br />';
-        }
-        ?>
-
-        <h2>Perulangan While</h2>
-        <?php
-        echo "Perulangan 1 sampai 10 <br />";
-        $i=1;
-        while ($i<=10) {
-            echo "Perulangan ke: " . $i . '<br />';
-            $i++;
-        }
-        ?>
-
-        <h2>Perulangan dowhile</h2>
-        <?php
-        echo "Perulangan 1 sampai 10 <br />";
-        $i=1;
-        do {
-            echo "Perulangan ke: " . $i . '<br />';
-            $i++;
-        } while ($i<=10);
-        ?>
     </body>
 </html>
